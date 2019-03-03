84d412d8dde9accfd733114a5126457fbd7d56da
Evan Fleet <evfleet@gmail.com>
2019-03-03 16:46:22 -0500

    setup typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import * as React from "react";
 import { render } from "react-dom";
 
-import App from "./components/App";
+const App = () => <div>Hello World</div>;
 
 render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
+const path = require("path");
 const HTMLPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: path.join(__dirname, "/src/index.tsx"),
+  output: {
+    filename: "index.js",
+    path: path.join(__dirname, "/build")
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        test: /\.(ts|tsx)?$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
       }
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
   },
   plugins: [new HTMLPlugin({ template: "./src/index.html" })]
 };
